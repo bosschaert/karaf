b8d5d75c28292402a4bee0327473f8eeabb0c632
David Bosschaert <david.bosschaert@gmail.com>
2013-08-26 15:33:04 +0100

    More tests

--- service/guard/src/test/java/org/apache/karaf/service/guard/impl/GuardProxyCatalogTest.java
+++ service/guard/src/test/java/org/apache/karaf/service/guard/impl/GuardProxyCatalogTest.java
@@ -16,15 +16,25 @@
  */
 package org.apache.karaf.service.guard.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.easymock.EasyMock;
+import org.easymock.IAnswer;
 import org.junit.Test;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Filter;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 
 public class GuardProxyCatalogTest {
-
     @Test
     public void testGuardProxyCatalog() throws Exception {
         BundleContext bc = EasyMock.createNiceMock(BundleContext.class);
@@ -39,6 +49,70 @@ public class GuardProxyCatalogTest {
         GuardProxyCatalog gpc = new GuardProxyCatalog(bc);
         assertTrue("Service Tracker for ConfigAdmin should be opened", gpc.configAdminTracker.getTrackingCount() != -1);
         assertTrue("Service Tracker for ProxyManager should be opened", gpc.proxyManagerTracker.getTrackingCount() != -1);
+
+        gpc.close();
+        assertEquals("Service Tracker for ConfigAdmin should be closed", -1, gpc.configAdminTracker.getTrackingCount());
+        assertEquals("Service Tracker for ProxyManager should be closed", -1, gpc.proxyManagerTracker.getTrackingCount());
     }
 
+    @Test
+    public void testIsProxy() throws Exception {
+        BundleContext bc = mockBundleContext();
+
+        GuardProxyCatalog gpc = new GuardProxyCatalog(bc);
+
+        Dictionary<String, Object> props = new Hashtable<String, Object>();
+        props.put(GuardProxyCatalog.PROXY_MARKER_KEY, 12L);
+        assertTrue(gpc.isProxy(mockServiceReference(props)));
+        assertFalse(gpc.isProxy(mockServiceReference(new Hashtable<String, Object>())));
+    }
+
+    @Test
+    public void testIsProxyForBundle() throws Exception {
+        BundleContext bc = mockBundleContext();
+
+        GuardProxyCatalog gpc = new GuardProxyCatalog(bc);
+
+        Bundle bundle = EasyMock.createMock(Bundle.class);
+        EasyMock.expect(bundle.getBundleId()).andReturn(42L).anyTimes();
+        EasyMock.replay(bundle);
+
+        BundleContext testBC = EasyMock.createMock(BundleContext.class);
+        EasyMock.expect(testBC.getBundle()).andReturn(bundle).anyTimes();
+        EasyMock.replay(testBC);
+
+        Dictionary<String, Object> props1 = new Hashtable<String, Object>();
+        props1.put(GuardProxyCatalog.PROXY_MARKER_KEY, 42L);
+        assertTrue(gpc.isProxyFor(mockServiceReference(props1), testBC));
+        Dictionary<String, Object> props2 = new Hashtable<String, Object>();
+        props2.put(GuardProxyCatalog.PROXY_MARKER_KEY, 43L);
+        assertFalse(gpc.isProxyFor(mockServiceReference(props2), testBC));
+        assertFalse(gpc.isProxyFor(mockServiceReference(new Hashtable<String, Object>()), testBC));
+    }
+
+    private BundleContext mockBundleContext() throws InvalidSyntaxException {
+        BundleContext bc = EasyMock.createNiceMock(BundleContext.class);
+        EasyMock.expect(bc.createFilter(EasyMock.isA(String.class))).andAnswer(new IAnswer<Filter>() {
+            @Override
+            public Filter answer() throws Throwable {
+                return FrameworkUtil.createFilter((String) EasyMock.getCurrentArguments()[0]);
+            }
+        }).anyTimes();
+        EasyMock.replay(bc);
+        return bc;
+    }
+
+    private ServiceReference<?> mockServiceReference(final Dictionary<String, Object> props) {
+        ServiceReference<?> sr = EasyMock.createMock(ServiceReference.class);
+        EasyMock.expect(sr.getPropertyKeys()).andReturn(
+                Collections.list(props.keys()).toArray(new String [] {})).anyTimes();
+        EasyMock.expect(sr.getProperty(EasyMock.isA(String.class))).andAnswer(new IAnswer<Object>() {
+            @Override
+            public Object answer() throws Throwable {
+                return props.get(EasyMock.getCurrentArguments()[0]);
+            }
+        }).anyTimes();
+        EasyMock.replay(sr);
+        return sr;
+    }
 }
