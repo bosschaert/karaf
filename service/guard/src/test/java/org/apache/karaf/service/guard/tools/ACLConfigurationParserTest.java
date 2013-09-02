60b9f9e48bc27b2265556500a49080fed717dcde
David Bosschaert <david.bosschaert@gmail.com>
2013-09-02 14:55:36 +0100

    Add Role-based access to any OSGi Service
    Role-based access is added to existing OSGi services by hiding the
    original service from a client through service registry hooks and
    presenting a proxy to the service that checks the required roles before
    an invocation is made.
    
    To enable the Role-based access for services, the service needs to match
    the filter in the karaf.secured.services system property (typically set in
    etc/system.properties), e.g:
      karaf.secured.services = (|(objectClass=org.acme*)(foo=bar))
    Only services that match this filter are secured, other services are
    left alone.
    
    Required roles are registered through the OSGi Configuration Admin
    Service. The service PID is not important. For secured services
    configurations with a service.guard property that matches are looked up.
    Then roles associated with the current method, its signature and/or the
    passed arguments are looked up. For example, take the following
    configuration:
      service.guard = (objectClass=org.acme.MyServiceAPI)
      myMethod = admin, manager
    If no matching definition for the method being invoked can be found a
    user with any role can invoke the method.
    As with the role-based JMX access, method signatures and arguments
    (either literal or regexp-based) can be matched. This commit migrates
    that code from the management/server module to service/guard from where
    the JMX module imports it.
    
    When roles are checked, these are obtained from the Subject associated
    with the current AccessControlContext. By default the karaf
    RolePrincipal is checked for, but other role implementations can be
    supported through the following syntax:
      my.custom.RoleImpl:someRole
    
    Includes many new unit tests.

---
+++ service/guard/src/test/java/org/apache/karaf/service/guard/tools/ACLConfigurationParserTest.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.service.guard.tools;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.apache.karaf.service.guard.tools.ACLConfigurationParser;
+import org.junit.Test;
+
+public class ACLConfigurationParserTest {
+    @Test
+    public void testParseRoles() {
+        assertEquals(Arrays.asList("some_role"),
+                ACLConfigurationParser.parseRoles(" some_role   "));
+        assertEquals(Arrays.asList("a","b","C"),
+                ACLConfigurationParser.parseRoles("a,b,C"));
+        assertEquals(Collections.emptyList(),
+                ACLConfigurationParser.parseRoles("# test comment"));
+    }
+
+    @Test
+    public void testGetRolesForInvocation() {
+        Dictionary<String, Object> config = new Hashtable<String, Object>();
+        config.put("foo", "r1, r2");
+        config.put("bar(java.lang.String, int)[/aa/,/42/]", "ra");
+        config.put("bar(java.lang.String, int)[/bb/,/42/]", "rb");
+        config.put("bar(java.lang.String, int)[\"cc\", \"17\"]", "rc");
+        config.put("bar(java.lang.String, int)", "rd");
+        config.put("bar(java.lang.String)", "re");
+        config.put("bar", "rf");
+
+        assertEquals(Arrays.asList("r1", "r2"), ACLConfigurationParser.getRolesForInvocation(
+                "foo", new Object [] {}, new String [] {}, config));
+        assertEquals(Arrays.asList("r1", "r2"), ACLConfigurationParser.getRolesForInvocation(
+                "foo", new Object [] {"test"}, new String [] {"java.lang.String"}, config));
+        assertNull(ACLConfigurationParser.getRolesForInvocation(
+                "test", new Object [] {}, new String [] {}, config));
+        assertEquals(Arrays.asList("ra"), ACLConfigurationParser.getRolesForInvocation(
+                "bar", new Object [] {"aa", 42}, new String [] {"java.lang.String", "int"}, config));
+        assertEquals(Arrays.asList("rb"), ACLConfigurationParser.getRolesForInvocation(
+                "bar", new Object [] {"bb", 42}, new String [] {"java.lang.String", "int"}, config));
+        assertEquals(Arrays.asList("rc"), ACLConfigurationParser.getRolesForInvocation(
+                "bar", new Object [] {"cc", 17}, new String [] {"java.lang.String", "int"}, config));
+        assertEquals(Arrays.asList("rd"), ACLConfigurationParser.getRolesForInvocation(
+                "bar", new Object [] {"aaa", 42}, new String [] {"java.lang.String", "int"}, config));
+        assertEquals(Arrays.asList("re"), ACLConfigurationParser.getRolesForInvocation(
+                "bar", new Object [] {"aa"}, new String [] {"java.lang.String"}, config));
+        assertEquals(Arrays.asList("rf"), ACLConfigurationParser.getRolesForInvocation(
+                "bar", new Object [] {42}, new String [] {"int"}, config));
+    }
+}
